fa2f87d19b086abd62cd3bb043d8743ef853cd80
Valentin Ferron <ferron.vale@gmail.com>
2018-01-11 11:23:12 +0100

    change seed and add one user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.destroy_all
 Treatment.destroy_all
 
-print '///Creating 1 User...///'
+print '///Creating 2 User...///'
 val = User.create!(
   first_name: "Valentin",
   last_name: "Ferron",
@@ -11,8 +11,18 @@ val = User.create!(
   role: 2,
   admin: true,
   )
+
+valou = User.create!(
+  first_name: "Valou",
+  last_name: "Ferron",
+  phone_number: "06 75 84 96 75",
+  email: "valou@gmail.com",
+  password: "123123",
+  role: 0,
+  admin: false,
+  )
 print "👶 "
-print '///Created 1 User...///'
+print '///Created 2 User...///'
 
 print '///Creating 1 Treatment...///'
 traitement = Treatment.create!(
